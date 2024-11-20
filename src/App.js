d697971f41514b8bd1093960418bac2e375b22ee
dilankayasuru <dilankayasuru001@gmail.com>
2024-11-20 18:52:34 +0530

    test azure log

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
     const {setTheme} = useTheme();
 
     useEffect(() => {
+        console.log("testOnAzure");
         const serverUrl = process.env.REACT_APP_SERVER_URL;
         console.log(serverUrl);
         setTheme('light');
